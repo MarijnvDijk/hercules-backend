0e089f20a80da0b5c78f165731c9c6501f95e73d
Marijn van Dijk <marijn.vandijk@student.fontys.nl>
2023-04-30 21:19:18 +0200

    Change header

--- src/middlewares/info.middleware.ts
+++ src/middlewares/info.middleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
 function infoMiddleware(request: Request, response: Response, next: NextFunction) {
-    var ip = request.headers['x-forwarded-for'] || request.socket.remoteAddress;
+    var ip = request.headers['X-Real-IP'] || request.socket.remoteAddress;
     console.log(ip);
 
     next();
